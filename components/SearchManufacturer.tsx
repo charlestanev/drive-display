5ebbeba005d83132894369c6d03552121cda6658
ELMIR CHARLIE TANEV <51314768+charlestanev@users.noreply.github.com>
2024-08-29 21:53:37 +0300

    feat: Add lazy loading for car logo image in SearchManufacturer component

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,14 +1,36 @@
-import { Combobox, Transition } from '@headlessui/react'
+'use client';
+
+import { Combobox, ComboboxButton, ComboboxInput, Transition } from '@headlessui/react'
 import { SearchManufacturerProps } from '@/types'
 import React from 'react'
-
+import Image from 'next/image';
+import { useState } from 'react';
 
 const SearchManufacturer = ({
     manufacturer,
     setManufacturer
-}:SearchManufacturerProps) => {
+}: SearchManufacturerProps) => {
     return (
-        <div>SearchManufacturer</div>
+        <div className='search-manufacturer'>
+            <Combobox>
+                <div className="relative w-full">
+                    <ComboboxButton className="absolute top-[14px]">
+                        <Image
+                            src="/car-logo.svg"
+                            width={20}
+                            height={20}
+                            className="ml-4"
+                            alt="Car Logo"
+                        />
+                    </ComboboxButton>
+                    <ComboboxInput
+                        className="search-manufacturer__input"
+                        placeholder='Volkswagen'
+                        displayValue={(manufacturer:string) => manufacturer}
+                    />
+                </div>
+            </Combobox>
+        </div>
     )
 }
 
