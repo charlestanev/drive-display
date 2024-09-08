867d3e10bea2220bab4b6da25dd03285e5b85b07
ELMIR CHARLIE TANEV <51314768+charlestanev@users.noreply.github.com>
2024-09-08 18:19:46 +0300

    feat: Update CarCard component with image lazy loading and additional details

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -6,8 +6,12 @@ const CustomButton = ({
     title,
     containerStyles,
     handleClick,
-    btnType
-} : CustomButtonProps) => {
+    btnType,
+    textStyles,
+    rightIcon,
+
+
+}: CustomButtonProps) => {
     return (
         <button
             disabled={false}
@@ -15,9 +19,19 @@ const CustomButton = ({
             className={`custom-btn ${containerStyles}`}
             onClick={handleClick}
         >
-            <span className={`flex-1`}>
+            <span className={`flex-1 ${textStyles}`}>
                 {title}
             </span>
+            {rightIcon && (
+                <div className="relative w-6 h-6">
+                    <Image
+                        src={rightIcon}
+                        alt="right icon"
+                        fill
+                        className="object-contain"
+                    />
+                </div>
+            )}
         </button>
     );
 };
