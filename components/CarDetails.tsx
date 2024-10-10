225ca34197adfa940fef99833e3969d75dece609
ELMIR CHARLIE TANEV <51314768+charlestanev@users.noreply.github.com>
2024-10-10 09:27:03 +0300

    refactor: Improve CarDetails component UI and add image lazy loading
    Update the CarDetails component to enhance the user interface and introduce image lazy loading. The dialog panel now has a rounded-2xl style with increased padding. Additionally, the car details section now displays the make and model in a capitalized format. The car details are presented in a flex layout with a gap of 4 between each item. The keys are transformed to capitalize and the values are displayed in a bold font. These changes aim to improve the overall user experience and make the component more visually appealing.

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -39,7 +39,7 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                                 leaveFrom="opacity-100 scale-100"
                                 leaveTo="opacity-0 scale-95"
                             >
-                                <Dialog.Panel className="relative w-full max-w-lg max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white text-left shadow-xsl transition-all flex flex-col gap-5">
+                                <Dialog.Panel className="relative w-full max-w-lg max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white p-6 text-left shadow-xl transition-all flex flex-col gap-5">
                                     <button
                                         type="button"
                                         onClick={closeModal}
@@ -70,7 +70,22 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                                             </div>
                                         </div>
                                     </div>
-                                    
+
+                                    <div className="flex-1 flex flex-col gap-2">
+                                        <h2 className="font-semibold text-xl capitalize">
+                                            {car.make} {car.model}
+                                        </h2>
+                                        <div className='mt-3 flex flex-wrap gap-4'>
+                                            {Object.entries(car).map(([key, value]) => (
+                                                <div
+                                                    className='flex justify-between gap-5 w-full text-right'
+                                                    key={key}>
+                                                    <h4 className="text-grey capitalize">{key.split("_").join(" ")}</h4>
+                                                    <p className="text-black-100 font-semibold">{value}</p>
+                                                </div>
+                                            ))}
+                                        </div>
+                                    </div>
                                 </Dialog.Panel>
                             </Transition.Child>
                         </div>
--- utils/index.ts
+++ utils/index.ts
@@ -3,7 +3,7 @@ export async function fetchCars() {
         'x-rapidapi-key': 'b3119ead5cmsh6bd45153c8ca20cp1a2514jsnbb23656025fb',
         'x-rapidapi-host': 'cars-by-api-ninjas.p.rapidapi.com'
     }
-    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla',
+    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera',
         { headers: headers }
     );
 
