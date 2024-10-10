837c0c8d788461469d9b188af8e7a9924390f6a0
ELMIR CHARLIE TANEV <51314768+charlestanev@users.noreply.github.com>
2024-10-10 10:20:00 +0300

    refactor: Add generateCarImageUrl function to utils/index.ts and update CarCard component to use dynamic car image

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 import React, { useState } from 'react';
 import { CarProps } from '@/types';
 import { CarDetails, CustomButton } from '@/components';
-import { calculateUSCarRent } from '@/utils';
+import { calculateUSCarRent, generateCarImageUrl } from '@/utils';
 
 interface CarCardProps {
     car: CarProps;
@@ -36,7 +36,7 @@ const CarCard = ({ car }: CarCardProps) => {
             </p>
 
             <div className="relative w-full h-40 my-3 object-contain">
-                <Image src="/hero.png" alt="car model" fill priority className="object-contain" />
+                <Image src={generateCarImageUrl(car)} alt="car model" fill priority className="object-contain" />
             </div>
 
             <div className="relative flex w-full mt-2">
--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,5 @@
+import { CarProps } from "@/types";
+
 export async function fetchCars() {
     const headers = {
         'x-rapidapi-key': 'b3119ead5cmsh6bd45153c8ca20cp1a2514jsnbb23656025fb',
@@ -29,3 +31,17 @@ export const calculateUSCarRent = (city_mpg: number, year: number) => {
     return rentalRatePerDay.toFixed(0); // Return the rate rounded to the nearest whole number
 };
 
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+    const url = new URL("https://cdn.imagin.studio/getimage");
+    const { make, model, year } = car;
+
+    url.searchParams.append('customer', process.env.NEXT_PUBLIC_IMAGIN_API_KEY || '');
+    url.searchParams.append('make', make);
+    url.searchParams.append('modelFamily', model.split(" ")[0]);
+    url.searchParams.append('zoomType', 'fullscreen');
+    url.searchParams.append('modelYear', `${year}`);
+    // url.searchParams.append('zoomLevel', zoomLevel);
+    url.searchParams.append('angle', `${angle}`);
+
+    return `${url}`;
+} 
